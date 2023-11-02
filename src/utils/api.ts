c510c77a3474ebf98c483852b44da41082e0d75a
Andry Pebrianto <andrypeb227@gmail.com>
2023-11-02 13:28:24 +0700

    Add env & netlify.toml

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5425",
+  baseURL: process.env.API_URL,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      { find: "@threads", replacement: path.resolve(__dirname, "src/features/threads") },
-    ],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.API_URL": JSON.stringify(env.API_URL),
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: "@", replacement: path.resolve(__dirname, "src") },
+        {
+          find: "@threads",
+          replacement: path.resolve(__dirname, "src/features/threads"),
+        },
+      ],
+    },
+  };
 });
